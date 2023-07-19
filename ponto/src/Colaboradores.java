ba7aed87a112be091cdeb84a7067bff5e5345366
RafaelFariaGomes <rafa.skt.life@gmail.com>
2023-07-18 21:56:22 -0300

    Atualizações Classe Colaboradores

--- ponto/src/Colaboradores.java
+++ ponto/src/Colaboradores.java
@@ -1,48 +1,21 @@
 import java.util.Scanner;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Colaboradores {
-Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
     private String nome;
-    private  chegada, saidaAlmoco, voltaAlmoco, fimExpediente;
+    private String chegada, saidaAlmoco, voltaAlmoco, fimExpediente;
+    private String horaAtual;
+    private String Cargo;
 
-    void registro(){
+    void registro() {
         System.out.println("Digite seu nome:");
         nome = scan.nextLine();
-        System.out.println("Olá " + nome + " digite o horario de entrada.");
-        chegada = scan.nextInt();
-        System.out.println(nome + " seu horario de chagada foi as: " + chegada + " mesmo?");
+        LocalDateTime horaAtual = LocalDateTime.now();
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String chegada = horaAtual.format(formatter);
 
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getChegada() {
-        return chegada;
-    }
-    public void setChegada(int chegada) {
-        this.chegada = chegada;
-    }
-    public int getSaidaAlmoco() {
-        return saidaAlmoco;
-    }
-    public void setSaidaAlmoco(int saidaAlmoco) {
-        this.saidaAlmoco = saidaAlmoco;
-    }
-    public int getVoltaAlmoco() {
-        return voltaAlmoco;
-    }
-    public void setVoltaAlmoco(int voltaAlmoco) {
-        this.voltaAlmoco = voltaAlmoco;
-    }
-    public int getFimExpediente() {
-        return fimExpediente;
-    }
-    public void setFimExpediente(int fimExpediente) {
-        this.fimExpediente = fimExpediente;
+        System.out.println("seu ponto foi batido as: " + chegada);
     }
 }
--- ponto/src/Main.java
+++ ponto/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Colaboradores ana = new Colaboradores();
-        ana.setNome("Ana");
 
-        ana.registro();
     }
 
 }
