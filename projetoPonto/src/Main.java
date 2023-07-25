32909460305792925369d183568752ce5830a4c2
RafaelFariaGomes <rafa.skt.life@gmail.com>
2023-07-25 13:05:11 -0300

    Correção While

--- projetoPonto/src/Main.java
+++ projetoPonto/src/Main.java
@@ -5,15 +5,15 @@ public class Main {
         int escolha;
         Colaboradores colaborador1 = new Colaboradores();
 
-        System.out.printf("""
-                SISTEMA DE HORARIOS SMART CONECTA ltda
+        while (true){
+            System.out.printf("""
+                \nSISTEMA DE HORARIOS SMART CONECTA ltda
                 
                 se você já é um funcionario cadastrado no sistema de Horarios, digite 1
                 caso você é da grencia e deseja cadastrar um novo usuario, digite 2
                 para sair, digite 0
                 """);
-        escolha = scan.nextInt();
-        while (true){
+            escolha = scan.nextInt();
         if(escolha == 1){
         colaborador1.registro();
         //break;
